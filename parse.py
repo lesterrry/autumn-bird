f0898cf8631d4bd420fa81cd07569a88d8b08b70
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-10-01 21:24:09 +0300

    Works :)

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
 from sclib import SoundcloudAPI
+import json
+
+JSON_FILE = '/Users/lesterrry/Downloads/freq.json'
 
 def die(message):
 	print(f'FATAL: {message}')
@@ -11,4 +14,14 @@ client_id = api.client_id
 if client_id is None or len(client_id) < 10:
 	die('Client id is invalid')
 
+print(f'Fetched: {client_id}')
+
+with open(JSON_FILE, 'r+', encoding='utf-8') as f:
+	existing = json.load(f)
+	existing['sc_key'] = client_id
+	f.seek(0)
+	json.dump(existing, f, indent=2, ensure_ascii=False)
+	f.truncate()
+
+print('File written')
 
